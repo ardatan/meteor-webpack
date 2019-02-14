edc40ef960d6bdaddfde9a7bf5fe418046a56e21
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-02-14 16:37:37 -0500

    Release 0.0.8

--- atmosphere-packages/webpack/package.js
+++ atmosphere-packages/webpack/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'ardatan:webpack',
-    version: '0.0.6_1',
+    version: '0.0.8',
     summary: 'Webpack Integration for Meteor',
     git: 'https://github.com/ardatan/meteor-webpack',
     documentation: '../../README.md'
