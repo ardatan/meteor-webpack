c6081a2028b0921015aa0b3641e3c1fe6a4e38ff
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-03-14 12:08:24 -0400

    v0.0.9

--- atmosphere-packages/webpack-dev-middleware/package.js
+++ atmosphere-packages/webpack-dev-middleware/package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'ardatan:webpack-dev-middleware',
     debugOnly: true,
-    version: '0.0.8',
+    version: '0.0.9',
     summary: 'Webpack Dev Middleware for Meteor',
     git: 'https://github.com/ardatan/meteor-webpack',
     documentation: '../../README.md'
