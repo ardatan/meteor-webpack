1f6796743d226c6fb65e5cf4f5c16e24ef3c3c71
abecks <abecks@gmail.com>
2019-03-18 11:57:16 -0700

    Release 0.0.11

--- atmosphere-packages/webpack/package.js
+++ atmosphere-packages/webpack/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'ardatan:webpack',
-    version: '0.0.10',
+    version: '0.0.11',
     summary: 'Webpack Integration for Meteor',
     git: 'https://github.com/ardatan/meteor-webpack',
     documentation: '../../README.md'
